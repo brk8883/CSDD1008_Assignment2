40842f0ca753fc414b84201cb12a4d4d54ad04bf
raj <brk8883@gmail.com>
2024-05-23 19:45:41 -0400

    Added main.go and readme.md

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/brk8883/CSDD1008_Assignment2
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello from Raj Kumar Bhandari 500233313")
+}
